888d760dee7f0cda19cdef81bcb630c2e2d6726f
Nataliyas <7nataly7@ukr.net>
2017-09-20 16:07:12 +0300

    changed directory

---
+++ src/main/java/JavaBasics.java
@@ -0,0 +1,12 @@
+public class JavaBasics {
+
+    public static void main(String[] args) {
+        //Use the Car keyword to declare Car Class variable
+        JavaBasics Toyota;
+
+        //Initialize the Toyota variable with a new Car object
+        Toyota = new JavaBasics();
+    }
+};
+
+
