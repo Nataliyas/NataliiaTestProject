8b997d88725c4164a65fb5933f910ad29335c49b
Nataliyas <7nataly7@ukr.net>
2017-09-26 13:29:52 +0300

    removed config file

--- src/main/java/JavaBasics.java
+++ src/main/java/JavaBasics.java
@@ -1,12 +1,43 @@
-public class JavaBasics {
+import com.sun.deploy.net.protocol.chrome.ChromeURLConnection;
+
+import java.util.Scanner;
+
+
+
+
+
+public class JavaBasics{
 
     public static void main(String[] args) {
-        //Use the Car keyword to declare Car Class variable
-        JavaBasics Toyota;
 
-        //Initialize the Toyota variable with a new Car object
-        Toyota = new JavaBasics();
+
     }
-};
+        };
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Input the 1st number: ");
+//        int num1 = in.nextInt();
+//        System.out.print("Input the 2d number: ");
+//        int num2 = in.nextInt();
+//        int result = Math.max(num1, num2);
+//        System.out.print(result + "    ");
+//        System.out.print("Input the 3d number: ");
+//        int num3 = in.nextInt();
+//        int finalResult = Math.max(result,num3);
+//        System.out.print(finalResult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
